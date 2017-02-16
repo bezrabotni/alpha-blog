a5484b36a0d593e9306c7a2d87f3fccd66305a3c
bezrabotni <dajanovic.damir@gmail.com>
2017-02-16 09:52:14 +0100

    testovi i navigacija kategorije

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -7,15 +8,13 @@ class CategoriesController < ApplicationController
 
   def new
     @category = Category.new
-
   end
 
   def create
     @category = Category.new(category_params)
     if @category.save
       flash[:success] = "Category was created successfuly"
-     redirect_to categories_path
-
+      redirect_to categories_path
     else
       render 'new'
     end
@@ -30,6 +29,13 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !logged_in? || (logged_in? and !current_user.admin?)
+      flash[:danger] = "Only admins can perform that action!"
+      redirect_to categories_path
+    end
   end
 
 end
